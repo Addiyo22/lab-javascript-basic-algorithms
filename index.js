d4a0ed02b86c89a8d87e0c4b94e347a73954a11d
Aditya Raikar <raikaraditya22@gmail.com>
2024-04-12 00:36:49 +0200

    Solved Bonus 1

--- index.js
+++ index.js
@@ -47,20 +47,28 @@
         console.log('Yo, the navigator goes first, definitely');
     }
 
-    //bonus 1
-    let str = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pulvinar sagittis risus, non faucibus urna. Vestibulum eget pellentesque odio. Cras varius dolor ac pulvinar suscipit. Sed vel justo neque. Phasellus id dui dictum elit tempor egestas a non orci. Mauris malesuada elit nec eros ultrices mollis. Morbi sit amet felis nibh. Phasellus iaculis interdum eros, at cursus urna tristique nec. Suspendisse sem nisi, iaculis eget gravida ac, mattis ut mi. Aliquam sapien nisi, auctor posuere laoreet ac, viverra non leo. Maecenas sem erat, lobortis non lectus sed, dapibus interdum justo. Curabitur cursus, augue et maximus dignissim, nunc metus feugiat elit, a porta odio metus sit amet eros.
+    //Bonus 1
+    let longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pulvinar sagittis risus, non faucibus urna. Vestibulum eget pellentesque odio. Cras varius dolor ac pulvinar suscipit. Sed vel justo neque. Phasellus id dui dictum elit tempor egestas a non orci. Mauris malesuada elit nec eros ultrices mollis. Morbi sit amet felis nibh. Phasellus iaculis interdum eros, at cursus urna tristique nec. Suspendisse sem nisi, iaculis eget gravida ac, mattis ut mi. Aliquam sapien nisi, auctor posuere laoreet ac, viverra non leo. Maecenas sem erat, lobortis non lectus sed, dapibus interdum justo. Curabitur cursus, augue et maximus dignissim, nunc metus feugiat elit, a porta odio metus sit amet eros.
 
     Aenean dolor velit, fringilla imperdiet eleifend in, sagittis at erat. Pellentesque faucibus leo vel efficitur cursus. Quisque cursus non urna ut consectetur. Nullam blandit commodo metus, at ultricies turpis porta non. Proin at nunc feugiat, lacinia quam ac, finibus ante. Nam facilisis lacus vitae odio ultrices, et semper nisl maximus. Cras vel tortor nibh. Donec purus neque, condimentum ac venenatis id, molestie non nisl. Nullam eget lectus id ipsum iaculis commodo ac eu magna. Sed sed accumsan elit. Nulla quis mattis metus.
     
     Vestibulum at vulputate enim. Vivamus eu leo purus. Nunc vitae purus lacus. Maecenas eget sem congue, egestas nibh at, elementum felis. Aenean vitae lectus porttitor, iaculis odio a, dignissim dui. Suspendisse rhoncus magna in risus lobortis, ultricies consectetur sem fermentum. Donec ac metus facilisis, rutrum nisl a, rutrum odio. Donec posuere feugiat diam in tempus. Suspendisse cursus eu turpis in lobortis. Morbi rhoncus varius luctus. Mauris tempor quam tortor, sit amet egestas turpis tristique non. Donec eget nisl pretium, mollis sapien dictum, fringilla quam. Sed cursus molestie quam id suscipit.`;
 
-console.log(str);
+    let wordSearcher = longText.split(' ');
+    var etCount = 0;
+    console.log(wordSearcher.length);
+    for(i=0;i < wordSearcher.length;i++){
+        if(wordSearcher[i].includes("et")){
+            etCount++;
+        }
+    }
+    console.log(etCount);
 
     //Bonus 2
     let phraseToCheck = "step on no pets";
     let reversed = '';
     let reg = /[\W_]/g;
-    let strNew = phraseToCheck.toLowerCase().replace(reg, '');
+    let str = phraseToCheck.toLowerCase().replace(reg, '');
     for(let i = str.length - 1; i >= 0; i--){
         reversed += str[i];
     }
